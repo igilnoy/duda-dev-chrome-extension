77b9a552eb381edb1aeb62e65487dd79cc9b8a8c
igilnoy <igilnoy@gmail.com>
2021-05-24 18:45:13 +0300

    detect unoptimized images

--- content.js
+++ content.js
@@ -1,6 +1,31 @@
 
         const closeIcon  = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FF5666"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"/></svg>`;
 
+        function scanForUnoptimizedImages (doc) {
+            Array.from(doc.images)
+            .forEach(img => {
+                img.classList.add('unoptimized')
+                if(img.naturalWidth - img.clientWidth > 100 ) {
+                    img.title = `❌ image is not optimized, displayed width is ${img.clientWidth} but it's original width is ${img.naturalWidth}`
+                }
+            });
+        }
+
+        // function scanForUnoptimizedBgImages (doc) {
+        //     const srcChecker = /url\(\s*?['"]?\s*?(\S+?)\s*?["']?\s*?\)/i
+        //     return Array.from(
+        //         Array.from(doc.querySelectorAll('*'))
+        //         .forEach((node) => {
+        //             let prop = window.getComputedStyle(node, null)
+        //                 .getPropertyValue('background-image')
+        //             let match = srcChecker.exec(prop)
+        //             if (match) {
+        //                 collection.add(match[1])
+        //             }
+        //         })
+        //     )
+        // }
+
         function createWrapper() {
             const wrapper = document.createElement('div');
             wrapper.classList.add('duda-ext-wrapper');
@@ -57,7 +82,7 @@
             return a;
         }
 
-        async function render() {
+        async function renderInfoPanel() {
             const div = createWrapper();
             appendBadge(div, 'criticalCss-true-green?style=flat-square&logo=CSS3','criticalCss-false-red?style=flat-square&logo=CSS3')(!!document.getElementById('criticalCss'));
             const hasCustomCode = await getHasCustomCode();
@@ -86,7 +111,9 @@
         }
 
         if(!!document.getElementById('dmRoot')) {
-            render();
+            renderInfoPanel();
+            scanForUnoptimizedImages(document);
+            //scanForUnoptimizedBgImages(document)
         }
 
 
